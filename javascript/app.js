6840cdd7255e070db7bdc2f77fb538e3a427e40c
neyser zana <neyserj.zana@gmail.com>
2019-03-01 01:23:21 -0500

    adding body-parser

--- javascript/app.js
+++ javascript/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var electionRouter = require('./routes/elections');
@@ -18,6 +19,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+//* parse application/json
+app.use(bodyParser.json())
 
 app.use('/', indexRouter);
 app.use('/search', electionRouter);
