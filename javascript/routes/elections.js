b9723c07d561f969734503f5e8c3dee11550e793
neyser zana <neyserj.zana@gmail.com>
2019-03-12 18:40:39 -0400

    fixing election requests and displaying that on site

--- javascript/routes/elections.js
+++ javascript/routes/elections.js
@@ -2,27 +2,17 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 var us_states = require('../us_state.js');
+var createOcdUrl = require('../utils/createOcdUrl');
 
 // * Post matched elections page. *//
 router.post('/', function (req, res) {
-  var state = req.body.state.toLowerCase();
-  var city = req.body.city;
-  // * replacing spaces with underscores
-  city = city.replace(' ', '_').toLowerCase();
-
-  // * this is the full url with state and city information so we can make the API request
-  var url =
-    'https://api.turbovote.org/elections/upcoming?district-divisions=ocd-division/country:us/state:'
-    + state +
-    ',ocd-division/country:us/state:'
-    + state +
-    '/place:'
-    + city;
+  
+  var ocdUrl = createOcdUrl(req.body.state, req.body.city);
 
   // * This request will handle getting all upcoming elections. We set our header to receive json.
   request.get({
-    url: url,
-    header: {
+    url: ocdUrl,
+    headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json'
     }
@@ -33,13 +23,18 @@ router.post('/', function (req, res) {
       res.redirect('/');
     } else {
       // * Here we get the data and render the elections page with upcoming elections
-      var elections = body;
-      // * We're getting the data now
-      // ! Header set to accept 'application/json', but data doesn't look like json object?? type = string
-      console.log('BODY: ', body);
-      console.log('TYPE: ', typeof body);
-      
-      res.render('elections', { title: 'Find My Election' });
+      var elections = JSON.parse(body);
+      // var elections = [
+      //   {
+      //     'polling-place-url':'https://registration.elections.myflorida.com/CheckVoterStatus',
+      //     description: 'Gainesville Municipal Election',
+      //   },
+      //   {
+      //     'polling-place-url': 'https://services.sos.wv.gov/Elections/Voter/FindMyPollingPlace',
+      //     description: 'Ceredo Municipal Election'
+      //   }
+      // ]
+      res.render('elections', { title: 'Find My Election', elections: elections});
     }
   });
 });
